d6560b5448c348904f01ef6400a4e12f2db042c4
Chris Gomez <ultimate.rebellion0@gmail.com>
2022-08-25 14:05:44 -0500

    Add extra minimum qty detail

--- pages/discounts.js
+++ pages/discounts.js
@@ -205,6 +205,10 @@ export default function Discounts({jwt}) {
               <dt className="text-sm font-medium text-gray-500">Amount</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{d.attributes.amount}</dd>
             </div>
+            <div className="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-500">Minimum Quantity</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{d.attributes.minimum}</dd>
+            </div>
             <div className="bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt className="text-sm font-medium text-gray-500">Code</dt>
               <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">{d.attributes.code}</dd>
